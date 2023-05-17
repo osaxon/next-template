079cb71babf823044ecec48aae6ea3b8f7cac1fb
Oliver Saxon <oliversaxon@Olivers-MacBook-Air.local>
2023-05-17 21:04:12 +0700

    Added type safety config file for site configurations

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,4 +1,27 @@
-const siteConfig = {
+type NavButton = {
+    label: string;
+    href: string;
+    style: NavButtonStyle;
+};
+
+type NavButtonStyle = "default" | "outline";
+
+type NavLink = {
+    label: string;
+    href: string;
+};
+
+type SiteConfig = {
+    clientName: string;
+    clientLogo: string;
+    siteTitle: string;
+    siteDescription: string;
+    navLinks: NavLink[];
+    navButtons: NavButton[];
+    siteTheme: string;
+};
+
+const siteConfig: SiteConfig = {
     clientName: "Client name",
     clientLogo: "/logo.png",
     siteTitle: "Testing",
